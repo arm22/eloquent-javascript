ac26d40c4b3edb35a257ef91bef5dcdaea9d6747
Austin Meyers <meyersaustin22@gmail.com>
2016-12-13 15:22:22 -0800

    Lint solutions

--- solutions/ch2-solutions.js
+++ solutions/ch2-solutions.js
@@ -1,27 +1,29 @@
 // Exercise #1
-for (var line = "#"; line.length < 8; line += "#")
-    console.log(line);
+for (var line = '#'; line.length < 8; line += '#') { console.log(line); }
 
-//Exercise #2
+// Exercise #2
 for (var n = 1; n <= 100; n++) {
-    var output = "";
-    if (n % 3 == 0)
-        output += "Fizz";
-    if (n % 5 == 0)
-        output += "Buzz";
-    console.log(output || n);
+  var output = '';
+  if (n % 3 == 0) {
+    output += 'Fizz';
+  }
+  if (n % 5 == 0) {
+    output += 'Buzz';
+  }
+  console.log(output || n);
 }
 
 // Exercise #3
 var size = 8;
-var board = "";
+var board = '';
 for (var y = 0; y < size; y++) {
-    for (var x = 0; x < size; x++) {
-        if ((x + y) % 2 == 0)
-            board += " ";
-        else
-            board += "#";
-        }
-    board += "\n";
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += ' ';
+    } else {
+      board += '#';
+    }
+  }
+  board += '\n';
 }
 console.log(board);
--- solutions/ch3-solutions.js
+++ solutions/ch3-solutions.js
@@ -1,32 +1,42 @@
 // Exercise #1
-function min(a, b) {
-    if (a < b)
-        return a;
-    else
-        return b;
+function min (a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
 }
 
 // Exercise #2
-function isEven(n) {
-    if (n == 0)
-        return true;
-    else if (n == 1)
-        return false;
-    else if (n < 0)
-        return isEven(-n);
-    else
-        return isEven(n - 2);
+function isEven (n) {
+  if (n == 0) { return true; } else if (n == 1) {
+    return false;
+  } else if (n < 0) {
+    return isEven(-n);
+  } else { return isEven(n - 2); }
 }
 
 // Exercise #3
-function countChar(string, ch) {
-    var counted = 0;
-    for (var i = 0; i < string.length; i++)
-        if (string.charAt(i) == ch)
-            counted += 1;
-    return counted;
+function countChar (string, ch) {
+  var counted = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
 }
 
-function countBs(string) {
-    return countChar(string, "B");
+function countBs (string) {
+  return countChar(string, 'B');
 }
+
+console.log(min(0, 10));
+console.log(min(0, -10));
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-2));
+
+console.log(countBs('BBC'));
+console.log(countChar('kakkerlak', 'k'));

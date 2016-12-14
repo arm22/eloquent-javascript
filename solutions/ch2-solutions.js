f1aa2cd3cc75dc12c5531e7ad109ab38ae82ddc1
Austin Meyers <meyersaustin22@gmail.com>
2016-12-14 14:56:41 -0800

    Use === over ==

--- solutions/ch2-solutions.js
+++ solutions/ch2-solutions.js
@@ -4,10 +4,10 @@ for (var line = '#'; line.length < 8; line += '#') { console.log(line); }
 // Exercise #2
 for (var n = 1; n <= 100; n++) {
   var output = '';
-  if (n % 3 == 0) {
+  if (n % 3 === 0) {
     output += 'Fizz';
   }
-  if (n % 5 == 0) {
+  if (n % 5 === 0) {
     output += 'Buzz';
   }
   console.log(output || n);
@@ -18,7 +18,7 @@ var size = 8;
 var board = '';
 for (var y = 0; y < size; y++) {
   for (var x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0) {
+    if ((x + y) % 2 === 0) {
       board += ' ';
     } else {
       board += '#';

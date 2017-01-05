140e2a19a33a4ade7f218415b6ed939e7e65ef21
Austin Meyers <meyersaustin22@gmail.com>
2017-01-05 09:49:09 -0800

    Finish CH. 9 exercises

--- exercises/ch9-exercises.js
+++ exercises/ch9-exercises.js
@@ -45,3 +45,20 @@ function verify (regexp, yes, no) {
 // Exercise #2
 var text = "'I'm the cook,' he said, 'it's my job.'";
 console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
+
+// Exercise #3
+var number = /^(\+|-|)(\d+?\.?\d*|\.\d+?)(e(\+|-|)(\d+?))*$/i;
+
+// Tests:
+['1', '-1', '+15', '1.55', '.5', '5.', '1.3e2', '1E-4',
+  '1e+12'].forEach(function (s) {
+    if (!number.test(s)) {
+      console.log("Failed to match '" + s + "'");
+    }
+  });
+['1a', '+-1', '1.2.3', '1+1', '1e4.5', '.5.', '1f5',
+  '.'].forEach(function (s) {
+    if (number.test(s)) {
+      console.log("Incorrectly accepted '" + s + "'");
+    }
+  });

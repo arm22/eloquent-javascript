ec8d530c29df2516f90f3a32107b4573d09e5463
Austin Meyers <meyersaustin22@gmail.com>
2017-01-10 08:49:21 -0800

    Lint CH. 10 solutions

--- solutions/ch10-solutions.js
+++ solutions/ch10-solutions.js
@@ -1,17 +1,16 @@
 // Exercise #1
-var month = function() {
-  var names = ["January", "February", "March", "April", "May",
-               "June", "July", "August", "September", "October",
-               "November", "December"];
+var month = (function () {
+  var names = ['January', 'February', 'March', 'April', 'May',
+    'June', 'July', 'August', 'September', 'October',
+    'November', 'December'];
   return {
-    name: function(number) { return names[number]; },
-    number: function(name) { return names.indexOf(name); }
+    name: function (number) { return names[number]; },
+    number: function (name) { return names.indexOf(name); }
   };
-}();
+}());
 
-
-//Exercise #1
+// Exercise #1
 console.log(month.name(2));
 // → March
-console.log(month.number("November"));
+console.log(month.number('November'));
 // → 10
